ed12a4e97843e6986e2dba7de23c051b27e98fe3
Boomer Rogers <booms@maintosh-7.local>
2013-12-01 11:50:01 +0000

    Adding settings and wsgi files

--- DevyardWeb/DevyardWeb/settings.py
+++ DevyardWeb/DevyardWeb/settings.py
@@ -1,5 +1,7 @@
 # Django settings for DevyardWeb project.
 import os.path
+import dj_database_url
+
 PROJECT_DIR = os.path.dirname(__file__)
 
 DEBUG = True
@@ -12,20 +14,14 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config( default="sqlite:///db.sqlite" )
 }
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -63,7 +59,9 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles/')
+
+BASE_DIR = os.path.dirname( os.path.abspath(__file__) )
+STATIC_ROOT = "staticfiles"
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
