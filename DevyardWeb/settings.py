abe4bae6269f38333615cd561293a146fae11e7a
Boomer Rogers <booms@maintosh-7.local>
2013-12-01 17:38:47 +0000

    Changeing servers

--- DevyardWeb/settings.py
+++ DevyardWeb/settings.py
@@ -14,7 +14,14 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-        'default': dj_database_url.config(default='sqlite:///db.sqlite')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
 }
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
